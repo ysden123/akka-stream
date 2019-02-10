a3ff163cb25f9521ea2dd1b2866d85337684888b
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2019-02-10 11:03:05 +0200

    Flow, sink, calculator

---
+++ akka-stream-scala/src/main/scala/com/stulsoft/akka/stream/scala/first/steps/SourceRangeWithFlow.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019. Yuriy Stul
+ */
+
+package com.stulsoft.akka.stream.scala.first.steps
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.concurrent.ExecutionContextExecutor
+
+/** Simple flow: source -> flow -> sink
+  *
+  * @author Yuriy Stul
+  */
+object SourceRangeWithFlow extends App with LazyLogging {
+  logger.info("==>main")
+  val system = ActorSystem.create("SourceRangeWithFlow")
+  implicit val materializer: ActorMaterializer = ActorMaterializer.create(system)
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+  val source = Source[Int](1 to 10)
+  val flow = Flow[Int].map(i => i + 1)
+  val sink = Sink.foreach(println)
+
+  source
+    .via(flow)
+    .runWith(sink)
+    .onComplete(_ => system.terminate)
+
+  logger.info("<==main")
+}
---
+++ akka-stream-scala/src/main/scala/com/stulsoft/akka/stream/scala/first/steps/SourceRangeWithSink.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019. Yuriy Stul
+ */
+
+package com.stulsoft.akka.stream.scala.first.steps
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.concurrent.ExecutionContextExecutor
+
+/** Simple flow: source -> sink
+  *
+  * @author Yuriy Stul
+  */
+object SourceRangeWithSink extends App with LazyLogging {
+  logger.info("==>main")
+  val system = ActorSystem.create("SourceRangeWithSink")
+  implicit val materializer: ActorMaterializer = ActorMaterializer.create(system)
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+  val source = Source[Int](1 to 10)
+  val sink = Sink.foreach(println)
+
+  source
+    .runWith(sink)
+    .onComplete(_ => system.terminate)
+
+  logger.info("<==main")
+}
